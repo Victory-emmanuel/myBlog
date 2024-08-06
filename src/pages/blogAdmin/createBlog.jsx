1f54818fe2d3d7b49b11be4055e720840a375506
Victory Emmanuel <marketinglot.blog@gmail.com>
2024-08-06 23:04:20 +0100

    commit

--- src/pages/blogAdmin/createBlog.jsx
+++ src/pages/blogAdmin/createBlog.jsx
@@ -74,7 +74,7 @@ const CreateBlog = () => {
 
   return (
     <div className="container h-full mx-auto max-w-5xl py-6">
-      <h1 className="text-accent font-bold">Create Blog</h1>
+      <h1 className="text-accent font-bold text-4xl my-16">Create Blog</h1>
       {/* main content */}
       <div className="mb-4">
         {/* thumbnail */}
@@ -199,7 +199,7 @@ const CreateBlog = () => {
         <h1 className="text-3xl">Preview</h1>
         <div className="content">
           <div
-            className={`[&>h1]:ss:text-[32px] [&>h1]:xx:text-[20px] [&>h1]:font-bold [&>h1]:mb-2.5 [&>h2]:text-[24px] [&>h2]:font-bold [&>h2]:mb-2.5 [&>h3]:text-[18.75px] [&>h3]:font-bold [&>h3]:mb-2.5 [&>h4]:text-[16px] [&>h4]:font-bold [&>h4]:mb-2.5 [&>h5]:text-[13.28px] [&>h5]:font-bold [&>h5]:mb-2.5 [&>h6]:text-[10px] [&>h6]:font-bold [&>h6]:mb-2.5 [&>p]:text-[16px] [&>p]:mb-1.5 [&>ul]:list-disc [&>ul]:mb-2 [&>ol]:list-decimal [&>ol]:mb-2 [&>li]:list-decimal [&>li]:mb-10 [&>img]:rounded-lg [&>img]:w-[100%]`}
+            className={`[&>h1]:ss:text-[32px] [&>h1]:xx:text-[20px] [&>h1]:font-bold [&>h1]:mb-2.5 [&>h2]:ss:text-[24px] [&>h2]:xx:text-[18.75px] [&>h2]:font-bold [&>h2]:mb-2.5 [&>h3]:text-[18.75px] [&>h3]:font-bold [&>h3]:mb-2.5 [&>h4]:text-[16px] [&>h4]:font-bold [&>h4]:mb-2.5 [&>h5]:text-[13.28px] [&>h5]:font-bold [&>h5]:mb-2.5 [&>h6]:text-[10px] [&>h6]:font-bold [&>h6]:mb-2.5 [&>p]:text-[16px] [&>p]:mb-1.5 [&>ul]:list-disc [&>ul]:mb-2 [&>ol]:list-decimal [&>ol]:mb-2 [&>li]:list-decimal [&>li]:mb-10 [&>img]:rounded-lg [&>img]:w-[100%]`}
             dangerouslySetInnerHTML={createMarkup(blogs.content)}
           ></div>
         </div>
